0787e112fec8ad92cde5d72e4e0baa9cbc69a779
Dickyk12 <71018224+Dickyk12@users.noreply.github.com>
2020-09-13 08:04:24 +0700

    program pilih pesanan

---
+++ pilih pesanan/pilih_pesanan.js
@@ -0,0 +1,34 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAnX8idR3pBq5LsJ1L7dbcxQFk8T_WtG9c",
+    authDomain: "pilih-pesanan.firebaseapp.com",
+    databaseURL: "https://pilih-pesanan.firebaseio.com",
+    projectId: "pilih-pesanan",
+    storageBucket: "pilih-pesanan.appspot.com",
+    messagingSenderId: "829313074769",
+    appId: "1:829313074769:web:a5442ab3780c46deb89a70"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function add_task(){
+    console.log("add_task");
+}
+
+function task_done(){
+    console.log("task_done");
+}
+
+function task_edit(){
+    console.log("task_edit");
+}
+
+function task_delete(){
+    console.log("task_delete");
+}
+
+
+
+
+  
+  
